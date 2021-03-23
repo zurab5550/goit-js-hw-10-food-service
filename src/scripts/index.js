dfe8aadfa47a9e5353dcf224378c30117121b0db
Maksim Shipovskiy <m.shipovskiy@gmail.com>
2021-03-23 18:50:32 +0300

    made swith light/dark theme

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,21 @@
 import cards from "./menu.json"
 import cardCreateFunction from "../html/templates/card.hbs"
+import { brotliDecompressSync } from "zlib"
 
 const cardList = document.querySelector(".js-menu")
 const cardHTMLTemplate = cardCreateFunction(cards)
 
 cardList.insertAdjacentHTML("beforeend", cardHTMLTemplate)
+
+const buttonChangeTheme = document.querySelector(".theme-switch__toggle")
+buttonChangeTheme.addEventListener("change", onClickChange)
+const mainBody = document.querySelector("body")
+function onClickChange() {
+  mainBody.classList.toggle("dark-theme")
+  console.log(mainBody.classList)
+  if (mainBody.classList[0] === "dark-theme") {
+    localStorage.setItem("theme", "dark")
+  } else {
+    localStorage.setItem("theme", "light")
+  }
+}
