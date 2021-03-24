881393131f1afb84fc032161e946d836aa7791ca
Maksim Shipovskiy <m.shipovskiy@gmail.com>
2021-03-24 09:47:48 +0300

    made localStorage

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,21 +1,38 @@
 import cards from "./menu.json"
 import cardCreateFunction from "../html/templates/card.hbs"
-import { brotliDecompressSync } from "zlib"
+// import { brotliDecompressSync } from "zlib"
 
 const cardList = document.querySelector(".js-menu")
 const cardHTMLTemplate = cardCreateFunction(cards)
 
 cardList.insertAdjacentHTML("beforeend", cardHTMLTemplate)
 
+const Theme = {
+  LIGHT: "light-theme",
+  DARK: "dark-theme",
+}
+
+const THEME_COLOR = "theme"
+
 const buttonChangeTheme = document.querySelector(".theme-switch__toggle")
 buttonChangeTheme.addEventListener("change", onClickChange)
 const mainBody = document.querySelector("body")
-function onClickChange() {
-  mainBody.classList.toggle("dark-theme")
-  console.log(mainBody.classList)
-  if (mainBody.classList[0] === "dark-theme") {
-    localStorage.setItem("theme", "dark")
+
+function onClickChange(e) {
+  if (e.target.checked) {
+    mainBody.classList.toggle(Theme.DARK)
+    localStorage.setItem(THEME_COLOR, Theme.DARK)
+
+    localStorage.getItem(THEME_COLOR)
   } else {
-    localStorage.setItem("theme", "light")
+    localStorage.setItem(THEME_COLOR, Theme.LIGHT)
+
+    mainBody.classList.toggle(Theme.LIGHT)
+    mainBody.classList.remove(Theme.DARK)
   }
+}
+
+if (localStorage.getItem(THEME_COLOR) === Theme.DARK) {
+  buttonChangeTheme.checked = true
+  mainBody.classList.add(Theme.DARK)
 }
